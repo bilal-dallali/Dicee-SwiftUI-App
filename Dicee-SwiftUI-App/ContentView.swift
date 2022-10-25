a0fd45c9fa69101f3b37d50bff1c4911b4f9b369
bilal-webdeveloper <dallali-bilal@hotmail.fr>
2022-10-25 21:57:58 +0200

    randomise dice

--- Dicee-SwiftUI-App/ContentView.swift
+++ Dicee-SwiftUI-App/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Spacer()
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 30))
+                        .fontWeight(.regular)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                        .padding(5)
+                }
+                .background(Color(red: 0.61, green: 0.11, blue: 0.12))
+                Spacer()
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    
+    let n: Int
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("dice\(n)")
+                .resizable()
+                .aspectRatio(1, contentMode: .fit)
+                .frame(width: 130, height: 130)
+                .padding()
         }
-        .padding()
     }
 }
 
