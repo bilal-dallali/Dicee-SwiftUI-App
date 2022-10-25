4f40faf32bb4a2ddf771ef0f6d0b280a1c0a0cc5
bilal-webdeveloper <dallali-bilal@hotmail.fr>
2022-10-25 15:42:07 +0200

    Initial Commit

---
+++ Dicee-SwiftUI-App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI-App
+//
+//  Created by Bilal Dallali on 25/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dicee-SwiftUI-App/Dicee_SwiftUI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUI_AppApp.swift
+//  Dicee-SwiftUI-App
+//
+//  Created by Bilal Dallali on 25/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
